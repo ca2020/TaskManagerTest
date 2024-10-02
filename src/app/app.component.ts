8db73a24be389071f69f6cf50a33c75ca0e0ab0d
Carlos Salcedo <broli2012_@hotmail.com>
2024-10-01 23:03:54 -0500

    implementacion de configuracion para estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {RouterOutlet} from "@angular/router";
+import {TaskFormComponent} from "./task-form/task-form.component";
+import {TaskListComponent} from "./task-list/task-list.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, TaskFormComponent, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
